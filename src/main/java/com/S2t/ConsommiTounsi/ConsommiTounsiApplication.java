c014b53f02e0d6dff61735115eadce34c5f373af
insafHermassi <insafhermassi27@gmail.com>
2023-01-20 11:33:54 +0100

    initiate spring project

---
+++ src/main/java/com/S2t/ConsommiTounsi/ConsommiTounsiApplication.java
@@ -0,0 +1,13 @@
+package com.S2t.ConsommiTounsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsommiTounsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsommiTounsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/S2t/ConsommiTounsi/ConsommiTounsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S2t.ConsommiTounsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsommiTounsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
